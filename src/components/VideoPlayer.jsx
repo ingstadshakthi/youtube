148f75f0d573bdb28a2085fc9d6a7614cbc3c672
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2021-08-24 11:32:12 +0530

    Add files via upload

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+function VideoPlayer() {
+  const url = localStorage.getItem("videoId");
+  return (
+    <div>
+      <ReactPlayer width="100%" height="90vh" controls url={url} onEnded={()=>{
+        window.location.href="/"
+      }} />
+    </div>
+  );
+}
+
+export default VideoPlayer;
